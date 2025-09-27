553d6826018a9230b9d90f4279bc406c28710ad0
YashMangla1 <yashmangla903@gmail.com>
2025-09-27 14:05:08 +0530

    Basic setup and Routing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { Routes, Route } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+    </Routes>
+  );
+}
+
+export default App;
